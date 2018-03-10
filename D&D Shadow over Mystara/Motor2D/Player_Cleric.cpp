5b468b7f12bb46b0b7b5363e44d96ffcd3afbd22
Alfonso Sanchez <alfonsosanchez1896@gmail.com>
2018-03-10 18:34:37 +0100

    class Cleric

---
+++ D&D Shadow over Mystara/Motor2D/Player_Cleric.cpp
@@ -0,0 +1,27 @@
+#include "Player_Cleric.h"
+
+
+
+Cleric::Cleric() : Entity(CLERIC)
+{
+	pugi::xml_document data;
+
+	if (pugi::xml_parse_result result = data.load_file("Entities.xml"))
+	{
+		pugi::xml_node anim = data.child("Entities").child("Cleric").child("Animations").child("Attack").child("frame");
+
+		while (anim != nullptr)
+		{
+			anim_attack.PushBack({ anim.attribute("x").as_int(), anim.attribute("y").as_int(), anim.attribute("w").as_int(), anim.attribute("h").as_int() });
+			anim = anim.next_sibling();
+		}
+
+		anim_attack.speed = anim.child("speed").attribute("speed").as_int();
+	}
+}
+
+Cleric::~Cleric()
+{
+
+}
+
---
+++ D&D Shadow over Mystara/Motor2D/Player_Cleric.h
@@ -0,0 +1,26 @@
+#ifndef _PLAYER_CLERIC_
+#define _PLAYER_CLERIC_
+
+
+#include "Entity.h"
+#include "ctAnimation.h"
+
+
+
+class Cleric :public Entity
+{
+public:
+	Cleric();
+	~Cleric();
+private:
+
+
+	ctAnimation anim_attack;
+	ctAnimation* current_animation = nullptr;
+
+
+};
+
+
+
+#endif

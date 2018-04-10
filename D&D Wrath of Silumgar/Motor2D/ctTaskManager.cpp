df40cbf956467962b692e4631be669b352b154df
Guillermo García Subirana <guillermogarsub@gmail.com>
2018-04-10 16:17:34 +0200

    dead ppl cant attack

--- D&D Wrath of Silumgar/Motor2D/ctTaskManager.cpp
+++ D&D Wrath of Silumgar/Motor2D/ctTaskManager.cpp
@@ -9,58 +9,64 @@ bool MoveToEntity::Execute()
 {
 	bool ret = false;
 
-	if (entity_to_go->GetCurrentHealthPoints() == 0) {
-		if (entity_to_move->type == ELF || entity_to_move->type == CLERIC || entity_to_move->type == WARRIOR || entity_to_move->type == DWARF) {
-			for (int i = 0; i < App->combat->enemies.size(); i++)
-			{
-				entity_to_go = App->combat->enemies.at(i);
-				if (entity_to_go->GetCurrentHealthPoints() != 0)
-					break;
+	if (entity_to_move->GetCurrentHealthPoints() != 0) {
+		if (entity_to_go->GetCurrentHealthPoints() == 0) {
+			if (entity_to_move->type == ELF || entity_to_move->type == CLERIC || entity_to_move->type == WARRIOR || entity_to_move->type == DWARF) {
+				for (int i = 0; i < App->combat->enemies.size(); i++)
+				{
+					entity_to_go = App->combat->enemies.at(i);
+					if (entity_to_go->GetCurrentHealthPoints() != 0)
+						break;
+				}
 			}
+			else {
+				for (int i = 0; i < App->combat->heroes.size(); i++)
+				{
+					entity_to_go = App->combat->heroes.at(i);
+					if (entity_to_go->GetCurrentHealthPoints() != 0)
+						break;
+				}
+			}
+		}
+
+
+		int x_objective = (entity_to_go->position.x + offset);
+
+		if (((entity_to_move->position.x + 25) >= x_objective && (entity_to_move->position.x - 25) <= x_objective) && ((entity_to_move->position.y + 25) >= entity_to_go->position.y && (entity_to_move->position.y - 25) <= entity_to_go->position.y))
+		{
+			entity_to_move->animation = &entity_to_move->idle;
+			ret = true;
 		}
 		else {
-			for (int i = 0; i < App->combat->heroes.size(); i++)
+			if (entity_to_move->position.x < entity_to_go->position.x)
 			{
-				entity_to_go = App->combat->heroes.at(i);
-				if (entity_to_go->GetCurrentHealthPoints() != 0)
-					break;
+				entity_to_move->animation = &entity_to_move->run_forward;
+				entity_to_move->position.x += BASE_SPEED;
+			}
+
+			else if (entity_to_move->position.x > entity_to_go->position.x)
+			{
+				entity_to_move->animation = &entity_to_move->run_forward;
+				entity_to_move->position.x -= BASE_SPEED;
+			}
+
+			if (entity_to_move->position.y < entity_to_go->position.y)
+			{
+				entity_to_move->animation = &entity_to_move->run_forward;
+				entity_to_move->position.y += BASE_SPEED;
+			}
+
+			else if (entity_to_move->position.y > entity_to_go->position.y)
+			{
+				entity_to_move->animation = &entity_to_move->run_forward;
+				entity_to_move->position.y -= BASE_SPEED;
 			}
 		}
 	}
-
-
-	int x_objective = (entity_to_go->position.x + offset);
-
-	if (((entity_to_move->position.x + 25) >= x_objective && (entity_to_move->position.x - 25) <= x_objective ) && ((entity_to_move->position.y + 25) >= entity_to_go->position.y && (entity_to_move->position.y - 25) <= entity_to_go->position.y))
-	{
-		entity_to_move->animation = &entity_to_move->idle;
+	else
 		ret = true;
-	}
-	else {
-		if (entity_to_move->position.x < entity_to_go->position.x)
-		{
-			entity_to_move->animation = &entity_to_move->run_forward;
-			entity_to_move->position.x += BASE_SPEED;
-		}
 
-		else if (entity_to_move->position.x > entity_to_go->position.x)
-		{
-			entity_to_move->animation = &entity_to_move->run_forward;
-			entity_to_move->position.x -= BASE_SPEED;
-		}
-
-		if (entity_to_move->position.y < entity_to_go->position.y)
-		{
-			entity_to_move->animation = &entity_to_move->run_forward;
-			entity_to_move->position.y += BASE_SPEED;
-		}
-
-		else if (entity_to_move->position.y > entity_to_go->position.y)
-		{
-			entity_to_move->animation = &entity_to_move->run_forward;
-			entity_to_move->position.y -= BASE_SPEED;
-		}
-	}
+	
 
 	return ret;
 }
@@ -71,41 +77,46 @@ bool MoveToInitialPosition::Execute()
 
 	//offset?
 
-	if (entity_to_move->flip_texture != true) {
-		entity_to_move->flip_texture = true;
-	}
+	if (entity_to_move->GetCurrentHealthPoints() != 0) {
+		if (entity_to_move->flip_texture != true) {
+			entity_to_move->flip_texture = true;
+		}
 
-	if (entity_to_move->position.x == entity_to_move->initial_position.x && entity_to_move->position.y == entity_to_move->initial_position.y)
-	{
-		entity_to_move->animation = &entity_to_move->idle;
-		entity_to_move->flip_texture = false;
+		if (entity_to_move->position.x == entity_to_move->initial_position.x && entity_to_move->position.y == entity_to_move->initial_position.y)
+		{
+			entity_to_move->animation = &entity_to_move->idle;
+			entity_to_move->flip_texture = false;
+			ret = true;
+		}
+		else {
+			if (entity_to_move->position.x < entity_to_move->initial_position.x)
+			{
+				entity_to_move->animation = &entity_to_move->run_forward;
+				entity_to_move->position.x += BASE_SPEED;
+			}
+
+			else if (entity_to_move->position.x > entity_to_move->initial_position.x)
+			{
+				entity_to_move->animation = &entity_to_move->run_forward;
+				entity_to_move->position.x -= BASE_SPEED;
+			}
+
+			if (entity_to_move->position.y < entity_to_move->initial_position.y)
+			{
+				entity_to_move->animation = &entity_to_move->run_forward;
+				entity_to_move->position.y += BASE_SPEED;
+			}
+
+			else if (entity_to_move->position.y > entity_to_move->initial_position.y)
+			{
+				entity_to_move->animation = &entity_to_move->run_forward;
+				entity_to_move->position.y -= BASE_SPEED;
+			}
+		}
+	}
+	else
 		ret = true;
-	}
-	else {
-		if (entity_to_move->position.x < entity_to_move->initial_position.x)
-		{
-			entity_to_move->animation = &entity_to_move->run_forward;
-			entity_to_move->position.x += BASE_SPEED;
-		}
-
-		else if (entity_to_move->position.x > entity_to_move->initial_position.x)
-		{
-			entity_to_move->animation = &entity_to_move->run_forward;
-			entity_to_move->position.x -= BASE_SPEED;
-		}
-
-		if (entity_to_move->position.y < entity_to_move->initial_position.y)
-		{
-			entity_to_move->animation = &entity_to_move->run_forward;
-			entity_to_move->position.y += BASE_SPEED;
-		}
-
-		else if (entity_to_move->position.y > entity_to_move->initial_position.y)
-		{
-			entity_to_move->animation = &entity_to_move->run_forward;
-			entity_to_move->position.y -= BASE_SPEED;
-		}
-	}
+	
 
 	return ret;
 }
@@ -117,52 +128,54 @@ bool PerformActionToEntity::Execute()
 
 	bool ret = false;
 
-	switch (action_to_perform.type)
-	{
-	case DEFAULT_ATTACK: {
+	if (actioner_entity->GetCurrentHealthPoints() != 0) {
+		switch (action_to_perform.type)
+		{
+		case DEFAULT_ATTACK: {
 
-		if (receiver_entity->GetCurrentHealthPoints() == 0) {
-			if (actioner_entity->type == ELF || actioner_entity->type == CLERIC || actioner_entity->type == WARRIOR || actioner_entity->type == DWARF) {
-				for (int i = 0; i < App->combat->enemies.size(); i++)
-				{
-					receiver_entity = App->combat->enemies.at(i);
-					if (receiver_entity->GetCurrentHealthPoints() != 0)
-						break;
+			if (receiver_entity->GetCurrentHealthPoints() == 0) {
+				if (actioner_entity->type == ELF || actioner_entity->type == CLERIC || actioner_entity->type == WARRIOR || actioner_entity->type == DWARF) {
+					for (int i = 0; i < App->combat->enemies.size(); i++)
+					{
+						receiver_entity = App->combat->enemies.at(i);
+						if (receiver_entity->GetCurrentHealthPoints() != 0)
+							break;
+					}
+				}
+				else {
+					for (int i = 0; i < App->combat->heroes.size(); i++)
+					{
+						receiver_entity = App->combat->heroes.at(i);
+						if (receiver_entity->GetCurrentHealthPoints() != 0)
+							break;
+					}
 				}
 			}
-			else {
-				for (int i = 0; i < App->combat->heroes.size(); i++)
-				{
-					receiver_entity = App->combat->heroes.at(i);
-					if (receiver_entity->GetCurrentHealthPoints() != 0)
-						break;
-				}
+
+			actioner_entity->animation = &actioner_entity->attack;
+
+			ret = actioner_entity->animation->Finished();
+
+			if (ret == true) {
+				actioner_entity->attack.Reset();
+				actioner_entity->animation = &actioner_entity->idle;
+
+				//TODO WE'RE NOT CALCULATING THE ATTACK AGAINST THE DEFENSE! also the action should have type of attack: fisical or magical!!!
+				receiver_entity->SetCurrentHealthPoints(receiver_entity->GetCurrentHealthPoints() + action_to_perform.health_points_effect);
+
+				//todo animate the receiver to hit + audio or smth
+
+				App->combat->UpdateHPBarOfEntity(receiver_entity, action_to_perform.health_points_effect);
 			}
 		}
-
-		actioner_entity->animation = &actioner_entity->attack;
-
-		ret = actioner_entity->animation->Finished();
-
-		if (ret == true) {
-			actioner_entity->attack.Reset();
-			actioner_entity->animation = &actioner_entity->idle;
-
-			//TODO WE'RE NOT CALCULATING THE ATTACK AGAINST THE DEFENSE! also the action should have type of attack: fisical or magical!!!
-			receiver_entity->SetCurrentHealthPoints(receiver_entity->GetCurrentHealthPoints() + action_to_perform.health_points_effect);
-
-			//todo animate the receiver to hit + audio or smth
-
-			App->combat->UpdateHPBarOfEntity(receiver_entity, action_to_perform.health_points_effect);
+							 break;
+		default:
+			break;
 		}
 	}
-		break;
-	default:
-		break;
-	}
+	else
+		ret = true;
 
-	
-	
 	return ret;
 }
 

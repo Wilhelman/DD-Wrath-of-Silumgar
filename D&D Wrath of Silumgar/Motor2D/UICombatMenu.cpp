9bc8895bc741359baaa1d367514098cef163f1c9
manelmourelo <manelmourelo.1@gmail.com>
2018-04-15 11:36:07 +0200

    Mindblown ability

--- D&D Wrath of Silumgar/Motor2D/UICombatMenu.cpp
+++ D&D Wrath of Silumgar/Motor2D/UICombatMenu.cpp
@@ -647,10 +647,15 @@ void UICombatMenu::SelectEnemy(std::vector<UIElement*> &current_vector) {
 		}
 		else if(abilities_label->current_state == STATE_EXECUTED && entity->abilities.size() != 0){
 			if (entity->GetCurrentManaPoints() >= entity->abilities.at(names_iterator).mana_points_effect_to_himself) {
-				//todo hacer guarrada
-				App->task_manager->AddTask(new MoveToEntity(entity, (*selected_enemy), -20));
-				App->task_manager->AddTask(new PerformActionToEntity(entity, entity->abilities.at(names_iterator), (*selected_enemy)));
-				App->task_manager->AddTask(new MoveToInitialPosition(entity));
+				if (entity->abilities.at(names_iterator).name == "Mindblown") {
+					App->task_manager->AddTask(new PerformActionToEntity(entity, entity->abilities.at(names_iterator), (*selected_ally)));
+					App->task_manager->AddTask(new MoveToInitialPosition(entity));
+				}
+				else {
+					App->task_manager->AddTask(new MoveToEntity(entity, (*selected_enemy), -20));
+					App->task_manager->AddTask(new PerformActionToEntity(entity, entity->abilities.at(names_iterator), (*selected_enemy)));
+					App->task_manager->AddTask(new MoveToInitialPosition(entity));
+				}
 			}
 			else{
 				selected_enemy = App->combat->enemies.begin();

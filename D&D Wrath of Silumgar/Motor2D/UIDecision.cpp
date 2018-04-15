4e81613cd643bad364fccc08965d4f227d877967
Guillermo García Subirana <guillermogarsub@gmail.com>
2018-04-15 18:54:57 +0200

    decision

--- D&D Wrath of Silumgar/Motor2D/UIDecision.cpp
+++ D&D Wrath of Silumgar/Motor2D/UIDecision.cpp
@@ -8,7 +8,7 @@ UIDecision::UIDecision(int x, int y, int decision_number, UI_Type type, UIElemen
 {
 	int size = 15;
 	std::string text = "Decided to set out on a new adventure with\n your weapons in hand, you leave the village without looking back, looking for the origin\n of the attack. But you stumble upon the first decision you must take which will mark your destiny and path.";
-	std::string text_options[] = { "!A. Follow the footprints that comes from\n the forest.", "!B.Take a shortcut through the cave,  dark, narrow and full of threats. Even the most experienced adventurers can   not get out without a wound. "};
+	std::string text_options[] = { "!A. Follow the footprints that comes from\n the forest.", "!B.Take a shortcut through the cave,  dark, narrow and full of threats."};
 	this->callback = callback;
 	
 

19f86ca5ce4df0865a553ab85a60325beb50cc90
VictorTirado <arlaquim@gmail.com>
2018-04-15 16:17:16 +0200

    Discard alpha in win/lose

--- D&D Wrath of Silumgar/Motor2D/UIVerticalSliceInfo.cpp
+++ D&D Wrath of Silumgar/Motor2D/UIVerticalSliceInfo.cpp
@@ -9,47 +9,28 @@ UIVerticalSliceInfo:: UIVerticalSliceInfo(int x, int y,bool overcome,ctModule* c
 	this->callback = callback;
 	
 	background = App->gui->AddUIImage(x, y, { 1141,484,340,178 },callback);
-	background->alpha = 0;
 	if (overcome == true)
 	{
 		congratulations = App->gui->AddUITextBox(x+20, y+20, 15, 200, "CONGRATULATIONS!!", {255,255,255,255},this, Second_Font);
-		congratulations->alpha = 0;
 		text = App->gui->AddUITextBox(x+20,y+50, 12,320,"You have managed to overcome the first part of this adventure. We hope you've enjoyed this gameplay. Thanks for playing and we hope to see you soon.", { 255,255,255,255 }, this, Second_Font);
-		text->alpha = 0;
 	}
 	else if (overcome == false)
 	{
 		congratulations = App->gui->AddUITextBox(x + 20, y + 20, 15, 200, "TRY AGAIN!!", { 255,255,255,255 }, this, Second_Font);
-		congratulations->alpha = 0;
 		text = App->gui->AddUITextBox(x + 20, y + 50, 12, 320, "Your efforts have not been enough to defeat Lich's troops, try again!", { 255,255,255,255 }, this, Second_Font);
-		text->alpha = 0;
 	}
 
 	
 }
 
 UIVerticalSliceInfo::~UIVerticalSliceInfo() {
-	background->to_destroy = true;
-	congratulations->to_destroy = true;
-	text->to_destroy = true;
+	App->gui->DeleteUIElement(*background);
+	background = nullptr;
+	App->gui->DeleteUIElement(*congratulations);
+	congratulations = nullptr;
+	App->gui->DeleteUIElement(*text);
+	text = nullptr;
 	callback = nullptr;
 	
 }
 
-void UIVerticalSliceInfo::Update()
-{
-	background->alpha += 3;
-	if (background->alpha >= 255)
-		background->alpha = 255;
-	congratulations->alpha += 3;
-	if (congratulations->alpha >= 255)
-		congratulations->alpha = 255;
-	text->alpha += 3;
-	if (text->alpha >= 255)
-		text->alpha = 255;
-
-	/*background->alpha -= 4;
-	if (background->alpha <= 0)
-		background->alpha = 0;*/
-
-}

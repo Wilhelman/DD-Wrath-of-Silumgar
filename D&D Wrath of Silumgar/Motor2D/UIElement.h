5fbd5f9b977ea2b8cb4f3bd35aa03bf0f83a85e7
Siitoo <alfonsosanchez1896@gmail.com>
2018-04-19 08:58:08 +0200

    Add comments to change

--- D&D Wrath of Silumgar/Motor2D/UIElement.h
+++ D&D Wrath of Silumgar/Motor2D/UIElement.h
@@ -23,7 +23,9 @@ enum UI_State {
 class UIElement
 {
 protected:
+
 	ctModule* callback = nullptr;
+
 	iPoint local_position = iPoint(0,0);
 
 	uint pressed_fx = 0u;
@@ -36,16 +38,22 @@ private:
 	UIElement* parent = nullptr;
 public:
 	
-	bool non_drawable = false;
 	bool to_destroy = false;
+
+	//Esto se podría poner en privado y poner una función que te diga el tipo con tal de que no se pueda trastear desde fuera.
 	UI_Type type = UI_NOT_DEF;
 	UI_State current_state = STATE_NO_DEF;
+	//----------------------------------------------
 
 	//Se puede poner en protected tocando los otros modulos sin hacer new.
 	//Y en privado si lo pintamos todo en el mismo blit
-	iPoint screen_position = iPoint(0, 0);
+		//Screen_position con local position se puede poner unicamente 1 de las dos variables, es innecesario 2.
+			iPoint screen_position = iPoint(0, 0);
+		//-----------------------------------------
 	SDL_Texture* texture = nullptr;
 	SDL_Rect current_rect = { 0,0,0,0 };
+	bool non_drawable = false;
+	//--------------------------------------------------
 	//--------------------------------------------------
 public:
 	UIElement(int x, int y, UI_Type type = UI_NOT_DEF, UIElement* parent = nullptr);
@@ -53,8 +61,10 @@ public:
 
 	virtual void Update();
 	virtual void Draw(SDL_Texture* sprites);
+	//Una de las dos la podemos borrar
 	iPoint GetLocalPosition() const;
 	iPoint GetScreenPosition() const;
+	//-------------------------------
 	SDL_Rect GetRect() const;
 
 	void SetParent(UIElement* parent);

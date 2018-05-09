8d2254b3cb7708b810cb08513f00f77428934e31
MAtaur00 <mtmtarres@gmail.com>
2018-05-09 17:21:52 +0200

    cleric attack

--- D&D Wrath of Silumgar/Motor2D/Cleric.cpp
+++ D&D Wrath of Silumgar/Motor2D/Cleric.cpp
@@ -28,7 +28,7 @@ Cleric::Cleric(int x, int y, EntityType type) : Entity(x, y, type) {
 			LoadAnimation(animations, &idle);
 		else if (tmp == "run")
 			LoadAnimation(animations, &run_forward);
-		else if (tmp == "mace_hit")
+		else if (tmp == "default_attack")
 			LoadAnimation(animations, &attack);
 		else if (tmp == "hit")
 			LoadAnimation(animations, &hit);

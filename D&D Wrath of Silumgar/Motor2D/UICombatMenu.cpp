a417b0887a8e6bc6d224a0a5de50b2f72f47a336
manelmourelo <manelmourelo.1@gmail.com>
2018-05-12 18:01:13 +0200

    Solving Bugs with labels colors

--- D&D Wrath of Silumgar/Motor2D/UICombatMenu.cpp
+++ D&D Wrath of Silumgar/Motor2D/UICombatMenu.cpp
@@ -581,19 +581,19 @@ void UICombatMenu::LoadAbilities() {
 	//names.push_back("Ability9");
 
 	if (names.size() >= 3) {
-		if (entity->GetCurrentManaPoints() >= entity->abilities.at(0).mana_points_effect_to_himself) {
+		if (entity->GetCurrentManaPoints() >= entity_actions.at(0).mana_points_effect_to_himself) {
 			abilities.push_back(App->gui->AddUILabel(backgroundPos.x + label1_pos.x, backgroundPos.y + label1_pos.y, names.at(0), { 255,255,255,255 }, font_size, nullptr, background,DEFAULT_FONT,true));
 		}
 		else {
 			abilities.push_back(App->gui->AddUILabel(backgroundPos.x + label1_pos.x, backgroundPos.y + label1_pos.y, names.at(0), { 255,0,0,255 }, font_size, nullptr, background,DEFAULT_FONT, true));
 		}
-		if (entity->GetCurrentManaPoints() >= entity->abilities.at(1).mana_points_effect_to_himself) {
+		if (entity->GetCurrentManaPoints() >= entity_actions.at(1).mana_points_effect_to_himself) {
 			abilities.push_back(App->gui->AddUILabel(backgroundPos.x + label2_pos.x, backgroundPos.y + label2_pos.y, names.at(1), { 255,255,255,255 }, font_size, nullptr, background,DEFAULT_FONT, true));
 		}
 		else {
 			abilities.push_back(App->gui->AddUILabel(backgroundPos.x + label2_pos.x, backgroundPos.y + label2_pos.y, names.at(1), { 255,0,0,0 }, font_size, nullptr, background, DEFAULT_FONT, true));
 		}
-		if (entity->GetCurrentManaPoints() >= entity->abilities.at(2).mana_points_effect_to_himself) {
+		if (entity->GetCurrentManaPoints() >= entity_actions.at(2).mana_points_effect_to_himself) {
 			abilities.push_back(App->gui->AddUILabel(backgroundPos.x + label3_pos.x, backgroundPos.y + label3_pos.y, names.at(2), { 255,255,255,255 }, font_size, nullptr, background, DEFAULT_FONT, true));
 		}
 		else {
@@ -602,13 +602,13 @@ void UICombatMenu::LoadAbilities() {
 		names_iterator = 2;
 	}
 	else if (names.size() == 2) {
-		if (entity->GetCurrentManaPoints() >= entity->abilities.at(0).mana_points_effect_to_himself) {
+		if (entity->GetCurrentManaPoints() >= entity_actions.at(0).mana_points_effect_to_himself) {
 			abilities.push_back(App->gui->AddUILabel(backgroundPos.x + label1_pos.x, backgroundPos.y + label1_pos.y, names.at(0), { 255,255,255,255 }, font_size, nullptr, background, DEFAULT_FONT, true));
 		}
 		else {
 			abilities.push_back(App->gui->AddUILabel(backgroundPos.x + label1_pos.x, backgroundPos.y + label1_pos.y, names.at(0), { 255,0,0,255 }, font_size, nullptr, background, DEFAULT_FONT, true));
 		}
-		if (entity->GetCurrentManaPoints() >= entity->abilities.at(1).mana_points_effect_to_himself) {
+		if (entity->GetCurrentManaPoints() >= entity_actions.at(1).mana_points_effect_to_himself) {
 			abilities.push_back(App->gui->AddUILabel(backgroundPos.x + label2_pos.x, backgroundPos.y + label2_pos.y, names.at(1), { 255,255,255,255 }, font_size, nullptr, background, DEFAULT_FONT, true));
 		}
 		else {
@@ -617,7 +617,7 @@ void UICombatMenu::LoadAbilities() {
 		names_iterator = 1;
 	}
 	else if (names.size() == 1) {
-		if (entity->GetCurrentManaPoints() >= entity->abilities.at(0).mana_points_effect_to_himself) {
+		if (entity->GetCurrentManaPoints() >= entity_actions.at(0).mana_points_effect_to_himself) {
 			abilities.push_back(App->gui->AddUILabel(backgroundPos.x + label1_pos.x, backgroundPos.y + label1_pos.y, names.at(0), { 255,255,255,255 }, font_size, nullptr, background, DEFAULT_FONT, true));
 		}
 		else {

dc901e76bc2cdbf592dd1553171f3e792a992ea1
Alfonso Sanchez <alfonsosanchez1896@gmail.com>
2018-05-12 13:56:50 +0200

    bug

--- D&D Wrath of Silumgar/Motor2D/ctTaskManager.cpp
+++ D&D Wrath of Silumgar/Motor2D/ctTaskManager.cpp
@@ -961,7 +961,7 @@ bool PerformActionToEntity::Execute()
 
 						bool critical = false;
 
-						int damage_to_deal = action_to_perform.health_points_effect - (10 - App->entities->GetWarrior()->GetCurrentStrengthPoints());
+						int damage_to_deal = action_to_perform.health_points_effect - (12 + App->entities->GetWarrior()->GetCurrentStrengthPoints());
 						float damage_reduction = (float)receiver_entity->GetCurrentPhysicalDefensePoints() / 100 * (float)damage_to_deal;
 						actioner_dexterity = actioner_dexterity / 10;
 						random_thousand_faces_die = (rand() % 100) + 1;
